019ff25e35960c2c7d53871491b30017e2534574
IlyasPatel <ilyasp@gmail.com>
2017-07-06 12:12:57 +0100

    Adding a little documentation to the 3rd party schedule service.

--- src/main/java/uk/co/qa/schedule/ScheduleService.java
+++ src/main/java/uk/co/qa/schedule/ScheduleService.java
@@ -7,6 +7,11 @@ import java.util.Set;
 
 public interface ScheduleService {
 
-    Map<Integer, ScheduleWindow> getSchedule(Set<Integer> allProgrammes) ;
+    /**
+     * Get's a map of schedules for the provided programme Ids
+     * @param programmeIds
+     * @return Programme id and schedule
+     */
+    Map<Integer, ScheduleWindow> getSchedule(Set<Integer> programmeIds) ;
 
 }
